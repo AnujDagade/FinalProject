cdb5e9f2fdc95c4f9756f7e3f8bec1a1eae6b058
Anuj Dagade <dagadeanuj@gmail.com>
2022-11-13 06:37:02 +0000

    yummy

--- app/src/main/java/mooc/vandy/java4android/calculator/logic/Divide.java
+++ app/src/main/java/mooc/vandy/java4android/calculator/logic/Divide.java
@@ -6,7 +6,7 @@ package mooc.vandy.java4android.calculator.logic;
 public class Divide {
     private Double div;
     private int num1, num2;
-    Integer excepFlag = 0;
+    public Integer excepFlag = 0;
 
     public Divide(int num1, int num2) {
         this.num1 = num1;
@@ -17,8 +17,9 @@ public class Divide {
         try {
             div = (double)num1/num2;
         }
-        catch (ArithmeticException e) {
+        catch (Exception e) {
             excepFlag = 1;
+
         }
     }
 
